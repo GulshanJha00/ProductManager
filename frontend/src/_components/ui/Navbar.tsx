76308a1a3156932692c06a6efdc18c22a189e3cb
GulshanJha00 <gk4763549@gmail.com>
2025-08-31 23:08:16 +0530

    navbar unresponsive

---
+++ frontend/src/_components/ui/Navbar.tsx
@@ -0,0 +1,21 @@
+import styles from "@/_styles/ui/navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.element}>
+        <div>
+          <h1>Project Manager</h1>
+        </div>
+
+       
+       <div className={styles.links}>
+        <span className={styles.link}>Login</span>
+        <span className={styles.link}>Logout</span>
+       </div>
+       </div>
+    </nav>
+  );
+};
+
+export default Navbar;
