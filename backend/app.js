b38474f6a264ba266651e9606e04197197b145ce
GulshanJha00 <gk4763549@gmail.com>
2025-09-02 00:59:06 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,13 @@ const productsRouter = require("./routes/products");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/", productsRouter);
